4751b24e644883e2c9b814b2785cc76d295c9515
Shelby Cohen <s.cohen131@gmail.com>
2017-05-07 19:46:25 -0400

    Trainer column instead of company, more db changes, and title change

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -15,22 +15,22 @@ insert into company (id,name) values (  8,'Trainer 8');
 insert into computer (id,name,introduced,discontinued,company_id) values (  1,'Shelby Cohen','student',null,1);
 insert into computer (id,name,introduced,discontinued,company_id) values (  2,'Toby Dragon','faculty',null,2);
 insert into computer (id,name,introduced,discontinued,company_id) values (  3,'John Barr','faculty',null, 2);
-insert into computer (id,name,introduced,discontinued,company_id) values (  4,'CM-5e','student',null,2);
-insert into computer (id,name,introduced,discontinued,company_id) values (  5,'CM-5','student',null, 2);
-insert into computer (id,name,introduced,discontinued,company_id) values (  6,'MacBook Pro','student',null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values (  7,'Apple IIe',null,null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values (  8,'Apple IIc',null,null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values (  9,'Apple IIGS',null,null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 10,'Apple IIc Plus',null,null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 11,'Apple II Plus',null,null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 12,'Apple III','1980-05-01','1984-04-01',1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 13,'Apple Lisa',null,null,1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 14,'CM-2',null,null,2);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 15,'Connection Machine','1987-01-01',null,2);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 16,'Apple II','1977-04-01','1993-10-01',1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 17,'Apple III Plus','1983-12-01','1984-04-01',1);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 18,'COSMAC ELF',null,null,3);
-insert into computer (id,name,introduced,discontinued,company_id) values ( 19,'COSMAC VIP','1977-01-01',null,3);
+insert into computer (id,name,introduced,discontinued,company_id) values (  4,'Client name1','student',null,2);
+insert into computer (id,name,introduced,discontinued,company_id) values (  5,'Client name2','student',null, 2);
+insert into computer (id,name,introduced,discontinued,company_id) values (  6,'Client name3','student',null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (  7,'Client name4',null,null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (  8,'Client name5',null,null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values (  9,'Client name6',null,null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 10,'Client name7',null,null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 11,'Client name8',null,null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 12,'Client name9','faculty','1984-04-01',1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 13,'Client name10',null,null,1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 14,'Client name11','faculty',null,2);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 15,'Client name12','faculty',null,2);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 16,'Client name13','faculty','1993-10-01',1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 17,'Client name14','faculty','1984-04-01',1);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 18,'Client name15','student',null,3);
+insert into computer (id,name,introduced,discontinued,company_id) values ( 19,'Client name16','student',null,3);
 
 
 # --- !Downs
